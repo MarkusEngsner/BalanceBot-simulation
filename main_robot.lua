48503c0ae84056f17059bdd61653ddc7676a3ed7
Markus Engsner <markus.engsner@gmail.com>
2019-12-14 17:52:18 +0100

    Implemented gyro interface and basic control algo

--- main_robot.lua
+++ main_robot.lua
@@ -1,6 +1,7 @@
 X = 1
 Y = 2
 Z = 3
+ACCELEROMETER_ANGLE_OFFSET = math.pi / 2
 
 function speedChange_callback(ui,id,newVal)
     speed=minMaxSpeed[1]+(minMaxSpeed[2]-minMaxSpeed[1])*newVal/100
@@ -8,7 +9,6 @@ end
 
 function sysCall_init()
     -- This is executed exactly once, the first time this script is executed
-    print('in init...')
     robBase=sim.getObjectAssociatedWithScript(sim.handle_self) -- this is bubbleRob's handle
     leftMotor=sim.getObjectHandle("motor_left") -- Handle of the left motor
     rightMotor=sim.getObjectHandle("motor_right") -- Handle of the right motor
@@ -27,44 +27,70 @@ function sysCall_init()
     graphHandle=simGetObjectHandle("acc_graph")
     acc_z = 0
     counter = 0
+    angle_gyro = 0
+
+
+    curr_time = 0
+    prev_time = sim.getSimulationTime()
+
 end
 
-function y_angle(acceleration)
-    local angle = math.atan(acceleration[Z] / acceleration[Y])
-    return angle
+
+function getSensorData(stringSignalName)
+    data = simGetStringSignal(stringSignalName)
+    if (data) then
+        return sim.unpackFloatTable(data)
+    else
+        return {0, 0, 0}
+    end
+end
+
+function isNan(x)
+    return x ~= x
+end
+
+function isInf(x)
 end
 
 function sysCall_actuation()
-    data = simGetStringSignal("accelerometerData")
-    if (data) then
-        acceleration = sim.unpackFloatTable(data)
-    end
-    if (acceleration) then
-        acc_z = acceleration[3]
-        simSetGraphUserData(graphHandle, "accelX", acceleration[1])
-        simSetGraphUserData(graphHandle, "accelY", acceleration[2])
-        simSetGraphUserData(graphHandle, "accelZ", acceleration[3])
-        angle_y = y_angle(acceleration)
-        --simSetUIButtonLabel(ui, 3, string.format("Angle_Y: %.4f", angle_y))
-    end
-    counter = counter + 1
-    if (counter == 100) then
-        print(acc_z)
-        counter = 0
-    end
+    set_point = 0
+    acceleration = getSensorData("accelerometerData")
+    gyroData = getSensorData("gyroscopeData")
+    simSetGraphUserData(graphHandle, "accelX", acceleration[1])
+    simSetGraphUserData(graphHandle, "accelY", acceleration[2])
+    simSetGraphUserData(graphHandle, "accelZ", acceleration[3])
+    angle_accelerometer = math.atan(acceleration[Z] / -acceleration[Y]) 
 
-    max_acc = 10
-    max_speed = 500
-    speed = acc_z / max_acc * max_speed
-    if (acc_z > 0) then
-        sim.setJointTargetVelocity(leftMotor, -speed)
-        sim.setJointTargetVelocity(rightMotor, -speed)
-    else
+
+    curr_time = sim.getSimulationTime()
+    dt = curr_time - prev_time -- Time increase in ms
+    angle_gyro = angle_gyro + (gyroData[X] * dt / 1)
+
+
+
+    max_speed = -10000
+
+    --angle = angle_gyro * 0.75 + angle_accelerometer * 0.25
+    angle = angle_gyro
+    error_term = set_point - angle
+    speed = math.min(error_term / 1 * max_speed)
+    if (counter == 0) then
+        print("Gyro: "..angle_gyro)
+        print("Acce: "..angle_accelerometer)
+        print("Angle: "..angle)
+        print("Error:"..error_term)
+        print("Speed: "..speed)
+        print("")
+        counter = 200
+    end
+    counter = counter - 1
+    if (not isNan(speed) and not isInf(speed)) then
         sim.setJointTargetVelocity(leftMotor, speed)
         sim.setJointTargetVelocity(rightMotor, speed)
     end
 
 
+    prev_time = curr_time
 end
 
 function sysCall_cleanup()
